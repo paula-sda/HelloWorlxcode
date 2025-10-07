5685c140a39e0c4cdd58a4d56c3acc9fe3898e16
Paula Saenz <paula_saenz@euneiz.com>
2025-10-07 18:56:25 +0200

    cambios en ejercicio

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -8,37 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let flechasegue = "flecha"
     
-    @IBOutlet weak var imgPrincipal: UIImageView!
-    @IBOutlet weak var felicitacion: UILabel!
-    @IBOutlet weak var imagen2: UIImageView!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        self.title = "Felicitación cumple"
-        felicitacion.text = "HAPPY BIRTHDAY !!!"
-    }
-    
-    @IBAction func botonClicar(_ sender: UIButton) {
-        print("boton clicado")
-        self.performSegue (withIdentifier: flechasegue, sender: self)
-    }
+    let BirthdaySegue = "showCumpleSegue"
     
 
+    @IBAction func clickCumple(_ sender: UIButton) {
+        print("ver tarjeta cumple")
+        self.performSegue(withIdentifier: BirthdaySegue, sender: self)
+    }
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == flechasegue{
-            let destination = segue.destination as! secondViewController
-            destination.username = "Paula"
-            
+        if segue.identifier == BirthdaySegue {
+            let destinationVC = segue.destination as! birthdayViewController
+        
         }
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+       
     }
- 
 
     
     
---
+++ HelloWorld/HelloWorld/birthdayViewController.swift
@@ -0,0 +1,22 @@
+//
+//  secondViewController.swift
+//  HelloWorld
+//
+//  Created by Student on 7/10/25.
+//
+
+import UIKit
+
+class birthdayViewController: UIViewController {
+
+    @IBOutlet weak var img2: UIImageView!
+    @IBOutlet weak var texto: UILabel!
+    @IBOutlet weak var img: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        texto.text = "Happy Birthday!!!"
+    }
+    
+
+}
